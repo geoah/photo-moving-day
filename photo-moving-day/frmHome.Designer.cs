8c82b67f71ee73312acd1accbce013670de795ae
George Antoniadis <george@noodles.gr>
2012-11-28 15:10:17 +0200

    Version 1.2
    First public release.
    Option: Copy/Move
    Option: Overwrite
    Option: Photographs/All files
    Option: Recursive search
    Option: Attribute date failover

---
+++ photo-moving-day/frmHome.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace photo_moving_day
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.folderBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtPathSource = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPathDestination = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSelectDestination = new System.Windows.Forms.Button();
+            this.chkMoveFiles = new System.Windows.Forms.CheckBox();
+            this.chkGetCreationDateOnExifFail = new System.Windows.Forms.CheckBox();
+            this.chkCategorizeAll = new System.Windows.Forms.CheckBox();
+            this.chkPathRecursive = new System.Windows.Forms.CheckBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chkOverwrite = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 212);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(215, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 0;
+            this.progressBar.Visible = false;
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted);
+            // 
+            // txtPathSource
+            // 
+            this.txtPathSource.Location = new System.Drawing.Point(15, 25);
+            this.txtPathSource.Name = "txtPathSource";
+            this.txtPathSource.Size = new System.Drawing.Size(262, 20);
+            this.txtPathSource.TabIndex = 1;
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.Location = new System.Drawing.Point(283, 25);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(26, 20);
+            this.btnSelectSource.TabIndex = 2;
+            this.btnSelectSource.Text = "...";
+            this.btnSelectSource.UseVisualStyleBackColor = true;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source path";
+            // 
+            // txtPathDestination
+            // 
+            this.txtPathDestination.Location = new System.Drawing.Point(15, 64);
+            this.txtPathDestination.Name = "txtPathDestination";
+            this.txtPathDestination.Size = new System.Drawing.Size(262, 20);
+            this.txtPathDestination.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination path";
+            // 
+            // btnSelectDestination
+            // 
+            this.btnSelectDestination.Location = new System.Drawing.Point(283, 64);
+            this.btnSelectDestination.Name = "btnSelectDestination";
+            this.btnSelectDestination.Size = new System.Drawing.Size(26, 20);
+            this.btnSelectDestination.TabIndex = 2;
+            this.btnSelectDestination.Text = "...";
+            this.btnSelectDestination.UseVisualStyleBackColor = true;
+            this.btnSelectDestination.Click += new System.EventHandler(this.btnSelectDestination_Click);
+            // 
+            // chkMoveFiles
+            // 
+            this.chkMoveFiles.AutoSize = true;
+            this.chkMoveFiles.Location = new System.Drawing.Point(16, 92);
+            this.chkMoveFiles.Name = "chkMoveFiles";
+            this.chkMoveFiles.Size = new System.Drawing.Size(149, 17);
+            this.chkMoveFiles.TabIndex = 5;
+            this.chkMoveFiles.Text = "Move files instead of copy";
+            this.chkMoveFiles.UseVisualStyleBackColor = true;
+            // 
+            // chkGetCreationDateOnExifFail
+            // 
+            this.chkGetCreationDateOnExifFail.AutoSize = true;
+            this.chkGetCreationDateOnExifFail.Location = new System.Drawing.Point(16, 138);
+            this.chkGetCreationDateOnExifFail.Name = "chkGetCreationDateOnExifFail";
+            this.chkGetCreationDateOnExifFail.Size = new System.Drawing.Size(287, 17);
+            this.chkGetCreationDateOnExifFail.TabIndex = 6;
+            this.chkGetCreationDateOnExifFail.Text = "Use file creation date when Exif contains no date taken";
+            this.chkGetCreationDateOnExifFail.UseVisualStyleBackColor = true;
+            // 
+            // chkCategorizeAll
+            // 
+            this.chkCategorizeAll.AutoSize = true;
+            this.chkCategorizeAll.Location = new System.Drawing.Point(16, 161);
+            this.chkCategorizeAll.Name = "chkCategorizeAll";
+            this.chkCategorizeAll.Size = new System.Drawing.Size(212, 17);
+            this.chkCategorizeAll.TabIndex = 7;
+            this.chkCategorizeAll.Text = "Categorize all files, not just photographs";
+            this.chkCategorizeAll.UseVisualStyleBackColor = true;
+            // 
+            // chkPathRecursive
+            // 
+            this.chkPathRecursive.AutoSize = true;
+            this.chkPathRecursive.Location = new System.Drawing.Point(16, 184);
+            this.chkPathRecursive.Name = "chkPathRecursive";
+            this.chkPathRecursive.Size = new System.Drawing.Size(224, 17);
+            this.chkPathRecursive.TabIndex = 8;
+            this.chkPathRecursive.Text = "Search for files recursively in source folder";
+            this.chkPathRecursive.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(237, 212);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(72, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // chkOverwrite
+            // 
+            this.chkOverwrite.AutoSize = true;
+            this.chkOverwrite.Location = new System.Drawing.Point(16, 115);
+            this.chkOverwrite.Name = "chkOverwrite";
+            this.chkOverwrite.Size = new System.Drawing.Size(197, 17);
+            this.chkOverwrite.TabIndex = 10;
+            this.chkOverwrite.Text = "Overwrite file if it exists in destination";
+            this.chkOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 245);
+            this.Controls.Add(this.chkOverwrite);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chkPathRecursive);
+            this.Controls.Add(this.chkCategorizeAll);
+            this.Controls.Add(this.chkGetCreationDateOnExifFail);
+            this.Controls.Add(this.chkMoveFiles);
+            this.Controls.Add(this.txtPathDestination);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectDestination);
+            this.Controls.Add(this.btnSelectSource);
+            this.Controls.Add(this.txtPathSource);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Photo Moving Day";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        protected System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowse;
+        private System.Windows.Forms.TextBox txtPathSource;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPathDestination;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelectDestination;
+        private System.Windows.Forms.CheckBox chkMoveFiles;
+        private System.Windows.Forms.CheckBox chkGetCreationDateOnExifFail;
+        private System.Windows.Forms.CheckBox chkCategorizeAll;
+        private System.Windows.Forms.CheckBox chkPathRecursive;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox chkOverwrite;
+    }
+}
+
---
+++ photo-moving-day/frmHome.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace photo_moving_day
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+            txtPathSource.Text = Properties.Settings.Default.pathSource;
+            txtPathDestination.Text = Properties.Settings.Default.pathDestination;
+            chkMoveFiles.Checked = Properties.Settings.Default.moveFiles;
+            chkGetCreationDateOnExifFail.Checked = Properties.Settings.Default.getCreationDateOnExifFail;
+            chkCategorizeAll.Checked = Properties.Settings.Default.categorizeAllFiles;
+            chkPathRecursive.Checked = Properties.Settings.Default.pathRecursive;
+            chkOverwrite.Checked = Properties.Settings.Default.overwriteFiles;
+        }
+
+        private void begin()
+        {
+            txtPathDestination.Enabled = false;
+            txtPathSource.Enabled = false;
+            chkCategorizeAll.Enabled = false;
+            chkGetCreationDateOnExifFail.Enabled = false;
+            chkMoveFiles.Enabled = false;
+            chkPathRecursive.Enabled = false;
+            chkOverwrite.Enabled = false;
+            btnStart.Text = "Stop";
+            progressBar.Visible = true;
+            bgWorker.RunWorkerAsync();
+        }
+
+        private void stop()
+        {
+            bgWorker.CancelAsync();
+        }
+
+        private void stopped()
+        {
+            txtPathDestination.Enabled = true;
+            txtPathSource.Enabled = true;
+            chkCategorizeAll.Enabled = true;
+            chkGetCreationDateOnExifFail.Enabled = true;
+            chkMoveFiles.Enabled = true;
+            chkOverwrite.Enabled = true;
+            chkPathRecursive.Enabled = true;
+            btnStart.Text = "Start";
+            progressBar.Visible = false;
+            progressBar.Value = 0;
+        }
+
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = (BackgroundWorker)sender;
+
+            String pathSource = txtPathSource.Text;
+            String pathDestination = txtPathDestination.Text;
+            Boolean filesMove = chkMoveFiles.Checked;
+            Boolean filesGetCreated = chkGetCreationDateOnExifFail.Checked;
+            Boolean filesCategorizeAll = chkCategorizeAll.Checked;
+            Boolean pathRecursive = chkPathRecursive.Checked;
+
+            if (pathSource == pathDestination)
+            {
+                MessageBox.Show("Source and destination folders are the same.");
+                return;
+            }
+            if (Directory.Exists(pathSource)==false)
+            {
+                MessageBox.Show("Source folder does not exist.");
+                return;
+            }
+            if (Directory.Exists(pathSource) == false)
+            {
+                MessageBox.Show("Destination folder does not exist.");
+                return;
+            }
+
+            StreamWriter logFile = File.AppendText("log-"+DateTime.Now.ToString("yyyyMMdd-HHmmss")+".txt");
+
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            String[] fileEntries;
+            if (pathRecursive == true)
+            {
+                fileEntries = Directory.GetFiles(pathSource, "*.*", SearchOption.AllDirectories);
+            }
+            else
+            {
+                fileEntries = Directory.GetFiles(pathSource, "*.*");
+            }
+            DialogResult dialogResult = MessageBox.Show("This will " + (chkMoveFiles.Checked ? "move" : "copy") + " " + (Properties.Settings.Default.categorizeAllFiles ? "all files" : "all images") + ".\r\n\r\nSource: " + Properties.Settings.Default.pathSource + "\r\nDestination: " + Properties.Settings.Default.pathDestination + "\r\n\r\nThere are " + fileEntries.Length.ToString() + " files present, this might take a while. Start?", "Photo Moving Day", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                logFile.WriteLine("Started on " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + ".");
+                logFile.WriteLine("Found a total of " + fileEntries.Length.ToString() + " files.");
+                logFile.Flush();
+                Int32 movedCount = 0;
+                Int32 totalCount = 0;
+                foreach (String filePath in fileEntries)
+                {
+                    if (worker.CancellationPending == true)
+                    {
+                        break;
+                    }
+                    try
+                    {
+                        DateTime datetimeTaken = (File.GetLastWriteTime(filePath) < File.GetCreationTime(filePath)) ? File.GetLastWriteTime(filePath) : File.GetCreationTime(filePath);
+                        Image image = null;
+                        try
+                        {
+                            image = new Bitmap(filePath);
+                        }
+                        catch (Exception excImage)
+                        {
+                            if (filesCategorizeAll == false)
+                            {
+                                throw excImage;
+                            }
+                        }
+                        if (image != null)
+                        {
+                            try
+                            {
+                                datetimeTaken = GetDateTaken(image);
+                            }
+                            catch (Exception excDt)
+                            {
+                                if (filesGetCreated == false)
+                                {
+                                    throw excDt;
+                                }
+                            }
+                            image.Dispose();
+                        }
+                        String pathDestinationFinal = Path.Combine(pathDestination, datetimeTaken.Year + "-" + datetimeTaken.Month.ToString().PadLeft(2, '0') + "-" + datetimeTaken.Day.ToString().PadLeft(2, '0'));
+                        if (!Directory.Exists(pathDestinationFinal))
+                        {
+                            Directory.CreateDirectory(pathDestinationFinal);
+                        }
+                        String fileName = Path.GetFileName(filePath);
+                        String pathDestinationFull = Path.Combine(pathDestinationFinal, fileName);
+                        Boolean fileExistsInDestination = File.Exists(pathDestinationFull);
+                        if (fileExistsInDestination == false || chkOverwrite.Checked == true)
+                        {
+                            if (fileExistsInDestination == true)
+                            {
+                                File.Delete(pathDestinationFull);
+                            }
+                            if (filesMove == true)
+                            {
+                                File.Move(filePath, pathDestinationFull);
+                                logFile.WriteLine("Moved \"" + filePath + "\" to \"" + pathDestinationFull + "\"");
+                            }
+                            else
+                            {
+                                File.Copy(filePath, pathDestinationFull);
+                                logFile.WriteLine("Copied \"" + filePath + "\" to \"" + pathDestinationFull + "\"");
+                            }
+                            movedCount += 1;
+                        }
+                        else
+                        {
+                            logFile.WriteLine("Warning: File \"" + filePath + "\" already exists as \"" + pathDestinationFull + "\"");
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        String error = "Error:  \"" + Path.GetFileName(filePath) + "\": " + exc.Message.ToString();
+                        logFile.WriteLine(error);
+                        logFile.Flush();
+                        Debug.Print(error);
+                    }
+                    totalCount += 1;
+                    double perc = (totalCount * 100) / fileEntries.Length;
+                    bgWorker.ReportProgress((int)Math.Floor(perc));
+                }
+                String done = "Moved around " + movedCount.ToString() + " photos.";
+                logFile.WriteLine(done);
+                logFile.WriteLine("");
+                MessageBox.Show(done);
+                logFile.Close();
+            }
+            bgWorker.ReportProgress(100);
+        }
+
+        private void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private DateTime GetDateTaken(Image targetImg)
+        {
+            //Property Item 36867 corresponds to the Date Taken
+            PropertyItem propItem = targetImg.GetPropertyItem(36867);
+            DateTime dtaken;
+
+            //Convert date taken metadata to a DateTime object
+            string sdate = Encoding.UTF8.GetString(propItem.Value).Trim();
+            string secondhalf = sdate.Substring(sdate.IndexOf(" "), (sdate.Length - sdate.IndexOf(" ")));
+            string firsthalf = sdate.Substring(0, 10);
+            firsthalf = firsthalf.Replace(":", "-");
+            sdate = firsthalf + secondhalf;
+            dtaken = DateTime.Parse(sdate);
+            return dtaken;
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //Application.Exit();
+            stopped();
+        }
+
+        private void btnSelectSource_Click(object sender, EventArgs e)
+        {
+            if (folderBrowse.ShowDialog() == DialogResult.OK)
+            {
+                txtPathSource.Text = folderBrowse.SelectedPath;
+            }
+        }
+
+        private void btnSelectDestination_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowse.ShowDialog() == DialogResult.OK)
+            {
+                txtPathDestination.Text = folderBrowse.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.pathSource = txtPathSource.Text;
+            Properties.Settings.Default.pathDestination = txtPathDestination.Text;
+            Properties.Settings.Default.moveFiles = chkMoveFiles.Checked;
+            Properties.Settings.Default.getCreationDateOnExifFail = chkGetCreationDateOnExifFail.Checked;
+            Properties.Settings.Default.categorizeAllFiles = chkCategorizeAll.Checked;
+            Properties.Settings.Default.pathRecursive = chkPathRecursive.Checked;
+            Properties.Settings.Default.overwriteFiles = chkOverwrite.Checked;
+            Properties.Settings.Default.Save();
+
+            if (btnStart.Text == "Start")
+            {
+                begin();
+            }
+            else
+            {
+                stop();
+            }
+        }
+    }
+}
